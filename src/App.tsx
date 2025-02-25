8b4a54a138ae45d0280dbfb8f1782d865ebe01f3
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-02-25 16:40:37 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ import { List } from "@alfalab/core-components/list";
 import { Comment } from "@alfalab/core-components/comment";
 import { Link } from "@alfalab/core-components/link";
 
-const aiLink = 'alfabank://configurable_chat?sourceChannelId=AI5_CHAT&navigationTitle=%D0%90%D0%BB%D1%8C%D1%84%D0%B0-%D0%90%D1%81%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BD%D1%82&attachmentsPickerEnabled=false&suggestionsEnabled=false&welcomeMessageEnabled=false&voiceMessageEnabled=false&quotesEnabled=false'
+const aiLink =
+  "alfabank://configurable_chat?sourceChannelId=AI5_CHAT&navigationTitle=%D0%90%D0%BB%D1%8C%D1%84%D0%B0-%D0%90%D1%81%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BD%D1%82&attachmentsPickerEnabled=false&suggestionsEnabled=false&welcomeMessageEnabled=false&voiceMessageEnabled=false&quotesEnabled=false";
 
 const Redirect = () => {
   window.location.href = aiLink;
@@ -89,12 +90,7 @@ export const App = () => {
         onClose={() => setWarning(false)}
         title="Внимание!"
         actionButton={
-          <ButtonMobile
-            block
-            view="primary"
-            onClick={submit}
-            href={aiLink}
-          >
+          <ButtonMobile block view="primary" onClick={submit} href={aiLink}>
             Понятно, продолжить
           </ButtonMobile>
         }
